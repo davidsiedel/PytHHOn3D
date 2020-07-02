e7a214e8bd24b14cac5c81e3b599c059c41c939c
davidsiedel <davidsiedel@gmail.com>
2020-07-02 13:42:07 +0200

    Changing gitignore and ingoring trash and drafts folders

--- drafts/draft_path_tester.py
+++
@@ -1,9 +0,0 @@
-from shapes.point import Point
-
-import sys, os
-from pathlib import Path
-
-current_folder = Path(os.path.dirname(os.path.abspath(__file__)))
-source = current_folder.parent
-package_folder = os.path.join(source, "pythhon3d")
-sys.path.insert(0, package_folder)
--- trash/draft_parser.py
+++
@@ -1,5 +0,0 @@
-from parsers.geof_parser import parse_geof_file
-
-geof_file_path = "/Users/davidsiedel/Projects/PytHHOn3D/meshes/squares_test.geof"
-# geof_file_path = "/Users/davidsiedel/Projects/PytHHOn3D/meshes/triangles_test.geof"
-parse_geof_file(geof_file_path)
