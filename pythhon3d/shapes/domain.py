82e1af3a02bd0942c49e456c11cb134f5534bf49
davidsiedel <davidsiedel@gmail.com>
2020-07-08 16:33:56 +0200

    Minor changes

--- pythhon3d/shapes/domain.py
+++ pythhon3d/shapes/domain.py
@@ -47,36 +47,3 @@ class Domain:
             domain_barycenter += vertex
         domain_barycenter = (1.0 / number_of_vertices) * domain_barycenter
         return domain_barycenter
-
-    # @staticmethod
-    # def get_domain_shape(vertices: Mat) -> str:
-    #     if vertices.shape == (1,):
-    #         domain_shape = "POINT"
-    #     if vertices.shape == (2,):
-    #         domain_shape = "SEGMENT"
-    #     if vertices.shape == (2,):
-    #     number_of_vertices = vertices.shape[0]
-    #     problem_dimension = vertices.shape[1]
-    #     if problem_dimension == 1 and number_of_vertices == 0:
-    #         shape == "POINT"
-    #     if problem_dimension == 1:
-    #         if
-    #     if number_of_vertices == 1 and problem_dimension == 1:
-    #         domain_shape = "POINT"
-    #     if number_of_vertices == 2 and problem_dimension == 1:
-    #         domain_shape = "SEGMENT"
-    #     if number_of_vertices == 3 and problem_dimension == 2:
-    #         domain_shape = "TRIANGLE"
-    #     if number_of_vertices == 4 and problem_dimension == 2:
-    #         domain_shape = "QUADRANGLE"
-    #     if number_of_vertices > 4 and problem_dimension == 2:
-    #         domain_shape = "POLYGON"
-    #     if number_of_vertices == 4 and problem_dimension == 3:
-    #         domain_shape = "TETRAHEDRON"
-    #     if number_of_vertices == 6 and problem_dimension == 3:
-    #         domain_shape = "PRISM"
-    #     if number_of_vertices == 8 and problem_dimension == 3:
-    #         domain_shape = "HEXAHEDRON"
-    #     if number_of_vertices > 8 and problem_dimension == 3:
-    #         domain_shape = "POLYHEDRON"
-    #     return domain_shape
