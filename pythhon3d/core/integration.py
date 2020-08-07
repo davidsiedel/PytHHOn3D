Fixing reconstructed gradient in the hdg file, 2D OK on triangular meshes

pythhon3d/core/integration.py
@@ -248,47 +248,47 @@ class Integration:
             face_mass_matrix_in_face += m
         return face_mass_matrix_in_face
 
-    @staticmethod
-    def get_face_displacement_vector_in_face(
-        face: Face, face_basis: Basis, face_reference_frame_transformation_matrix: Mat, displacement: Callable,
-    ) -> Mat:
-        """
-        ================================================================================================================
-        Description :
-        ================================================================================================================
-        
-        ================================================================================================================
-        Parameters :
-        ================================================================================================================
-        
-        ================================================================================================================
-        Exemple :
-        ================================================================================================================
-        
-        """
-        v_f = face.volume
-        x_f = face.centroid
-        # --------------------------------------------------------------------------------------------------------------
-        face_mass_matrix_in_face = np.zeros((face_basis.basis_dimension, face_basis.basis_dimension))
-        displacement_vector = np.zeros((face_basis.basis_dimension,))
-        # --------------------------------------------------------------------------------------------------------------
-        x_f_in_face = Face.get_points_in_face_reference_frame(face.centroid, face_reference_frame_transformation_matrix)
-        face_quadrature_nodes_in_face = Face.get_points_in_face_reference_frame(
-            face.quadrature_nodes, face_reference_frame_transformation_matrix
-        )
-        for x_Q_f_in_face, w_Q_f in zip(face_quadrature_nodes_in_face, face.quadrature_weights):
-            # ----------------------------------------------------------------------------------------------------------
-            psi_vector = face_basis.get_phi_vector(x_Q_f_in_face, x_f_in_face, v_f)
-            number_of_components = psi_vector.shape[0]
-            psi_vector = np.resize(psi_vector, (1, number_of_components))
-            # ----------------------------------------------------------------------------------------------------------
-            m = w_Q_f * psi_vector.T @ psi_vector
-            face_mass_matrix_in_face += m
-            # ----------------------------------------------------------------------------------------------------------
-            h = np.full((face_basis.basis_dimension,), displacement(x_Q_f_in_face))
-            displacement_vector += h
-        face_displacement_vector_in_face = np.linalg.inv(face_mass_matrix_in_face) @ displacement_vector.T
-        return face_displacement_vector_in_face
+    # @staticmethod
+    # def get_face_displacement_vector_in_face(
+    #     face: Face, face_basis: Basis, face_reference_frame_transformation_matrix: Mat, displacement: Callable,
+    # ) -> Mat:
+    #     """
+    #     ================================================================================================================
+    #     Description :
+    #     ================================================================================================================
+
+    #     ================================================================================================================
+    #     Parameters :
+    #     ================================================================================================================
+
+    #     ================================================================================================================
+    #     Exemple :
+    #     ================================================================================================================
+
+    #     """
+    #     v_f = face.volume
+    #     x_f = face.centroid
+    #     # --------------------------------------------------------------------------------------------------------------
+    #     face_mass_matrix_in_face = np.zeros((face_basis.basis_dimension, face_basis.basis_dimension))
+    #     displacement_vector = np.zeros((face_basis.basis_dimension,))
+    #     # --------------------------------------------------------------------------------------------------------------
+    #     x_f_in_face = Face.get_points_in_face_reference_frame(face.centroid, face_reference_frame_transformation_matrix)
+    #     face_quadrature_nodes_in_face = Face.get_points_in_face_reference_frame(
+    #         face.quadrature_nodes, face_reference_frame_transformation_matrix
+    #     )
+    #     for x_Q_f_in_face, w_Q_f in zip(face_quadrature_nodes_in_face, face.quadrature_weights):
+    #         # ----------------------------------------------------------------------------------------------------------
+    #         psi_vector = face_basis.get_phi_vector(x_Q_f_in_face, x_f_in_face, v_f)
+    #         number_of_components = psi_vector.shape[0]
+    #         psi_vector = np.resize(psi_vector, (1, number_of_components))
+    #         # ----------------------------------------------------------------------------------------------------------
+    #         m = w_Q_f * psi_vector.T @ psi_vector
+    #         face_mass_matrix_in_face += m
+    #         # ----------------------------------------------------------------------------------------------------------
+    #         h = np.full((face_basis.basis_dimension,), displacement(x_Q_f_in_face))
+    #         displacement_vector += h
+    #     face_displacement_vector_in_face = np.linalg.inv(face_mass_matrix_in_face) @ displacement_vector.T
+    #     return face_displacement_vector_in_face
 
     @staticmethod
     def get_face_pressure_vector_in_face(
@@ -323,6 +323,45 @@ class Integration:
             v = w_Q_f * psi_vector * pressure(x_Q_f_in_face)
             face_pressure_vector_in_face += v
         return face_pressure_vector_in_face
+
+    # @staticmethod
+    # def get_face_displacement_vector_in_face(
+    #     face: Face, face_basis: Basis, face_reference_frame_transformation_matrix: Mat, pressure: Callable,
+    # ) -> Mat:
+    #     """
+    #     ================================================================================================================
+    #     Description :
+    #     ================================================================================================================
+
+    #     ================================================================================================================
+    #     Parameters :
+    #     ================================================================================================================
+
+    #     ================================================================================================================
+    #     Exemple :
+    #     ================================================================================================================
+
+    #     """
+    #     v_f = face.volume
+    #     x_f = face.centroid
+    #     # --------------------------------------------------------------------------------------------------------------
+    #     face_pressure_vector_in_face = np.zeros((face_basis.basis_dimension,))
+    #     face_mass_matrix_in_face = np.zeros((face_basis.basis_dimension, face_basis.basis_dimension))
+    #     # --------------------------------------------------------------------------------------------------------------
+    #     x_f_in_face = Face.get_points_in_face_reference_frame(face.centroid, face_reference_frame_transformation_matrix)
+    #     face_quadrature_nodes_in_face = Face.get_points_in_face_reference_frame(
+    #         face.quadrature_nodes, face_reference_frame_transformation_matrix
+    #     )
+    #     for x_Q_f_in_face, w_Q_f in zip(face_quadrature_nodes_in_face, face.quadrature_weights):
+    #         # ----------------------------------------------------------------------------------------------------------
+    #         psi_vector = face_basis.get_phi_vector(x_Q_f_in_face, x_f_in_face, v_f)
+    #         v = w_Q_f * psi_vector * pressure(x_Q_f_in_face)
+    #         face_pressure_vector_in_face += v
+    #         # ----------------------------------------------------------------------------------------------------------
+    #         number_of_components = psi_vector.shape[0]
+    #         psi_vector = np.resize(psi_vector, (1, number_of_components))
+    #         face_mass_matrix_in_face += w_Q_f * psi_vector.T @ psi_vector
+    #     return face_pressure_vector_in_face
 
     @staticmethod
     def get_cell_load_vector_in_cell(cell: Cell, cell_basis: Basis, load: Callable) -> Mat:

pythhon3d/core/operators/hdg.py
@@ -45,7 +45,7 @@ class HDG(Operator):
         # --------------------------------------------------------------------------------------------------------------
         m_phi_phi_cell = Integration.get_cell_mass_matrix_in_cell(cell, cell_basis)
         m_phi_phi_cell_inv = np.linalg.inv(m_phi_phi_cell)
-        print("m_phi_phi_cell : \n{}".format(m_phi_phi_cell))
+        # print("m_phi_phi_cell : \n{}".format(m_phi_phi_cell))
         # --------------------------------------------------------------------------------------------------------------
         # Listing the field directions and derivative directions
         # --------------------------------------------------------------------------------------------------------------
@@ -102,8 +102,8 @@ class HDG(Operator):
                     # --------------------------------------------------------------------------------------------------
                     # Writing the field jump contribution in the gradient operator
                     # --------------------------------------------------------------------------------------------------
-                    local_gradient_operator[l0:l1, c0:c1] -= m_phi_phi_face
                     normal_vector_component = passmat[-1, j]
+                    local_gradient_operator[l0:l1, c0:c1] -= normal_vector_component * m_phi_phi_face
                     local_gradient_operator[l0:l1, f0:f1] += normal_vector_component * m_phi_psi_face
                     # local_gradient_operator[l0:l1, f0:f1] += m_phi_psi_face
                     # --------------------------------------------------------------------------------------------------
@@ -122,7 +122,7 @@ class HDG(Operator):
                     coef = 1.0 / 2.0
                 else:
                     coef = 1.0
-                print("coef : {}".format(coef))
+                # print("coef : {}".format(coef))
                 local_gradient_operator[l0:l1, :] = coef * m_phi_phi_cell_inv @ local_gradient_operator[l0:l1, :]
                 # print("m_stab_face : \n{}".format(m_stab_face))
         # print("local_gradient_operator : \n{}".format(local_gradient_operator))

pythhon3d/pythhon3d.py
@@ -84,6 +84,7 @@ def build(
         cells_faces_connectivity_matrix,
         cells_connectivity_matrix,
         nsets,
+        nsets_faces,
     ) = parse_mesh(mesh_file)
     # ------------------------------------------------------------------------------------------------------------------
     # Writing the vertices matrix as a numpy object
@@ -135,6 +136,7 @@ def build(
         cells_vertices_connectivity_matrix,
         faces_vertices_connectivity_matrix,
         nsets,
+        nsets_faces,
         cell_basis,
         face_basis,
         unknown,
@@ -150,6 +152,8 @@ def solve(
     cells_vertices_connectivity_matrix: Mat,
     faces_vertices_connectivity_matrix: Mat,
     nsets: dict,
+    # flags: List[str],
+    nsets_faces: dict,
     cell_basis: Basis,
     face_basis: Basis,
     unknown: Unknown,
@@ -202,7 +206,8 @@ def solve(
         for local_face_index, global_face_index in enumerate(local_faces_indices):
             face = faces[global_face_index]
             face_reference_frame_transformation_matrix = Operator.get_face_passmat(local_cell, face)
-            for boundary_name, nset in zip(nsets, nsets.values()):
+            # for boundary_name, nset in zip(nsets, nsets.values()):
+            for boundary_name, nset in zip(nsets_faces, nsets_faces.values()):
                 if connectivity[local_face_index] in nset:
                     pressure = boundary_conditions[boundary_name][1]
                     pressure_vector = Pressure(
@@ -282,12 +287,13 @@ def solve(
     # Displacement enforcement through Lagrange multiplier
     # ------------------------------------------------------------------------------------------------------------------
     number_of_constrained_faces = 0
-    for boundary_name, nset in zip(nsets, nsets.values()):
+    # for boundary_name, nset in zip(nsets, nsets.values()):
+    for boundary_name, nset in zip(nsets_faces, nsets_faces.values()):
+        print(boundary_name, nset)
         displacement = boundary_conditions[boundary_name][0]
-        if not displacement is None:
-            for displacement_component in displacement:
-                if not displacement_component is None:
-                    number_of_constrained_faces += len(nset)
+        for displacement_component in displacement:
+            if not displacement_component is None:
+                number_of_constrained_faces += len(nset)
     lagrange_multiplyer_matrix = np.zeros(
         (
             number_of_constrained_faces * face_basis.basis_dimension,
@@ -295,20 +301,35 @@ def solve(
         )
     )
     h_vector = np.zeros((number_of_constrained_faces * face_basis.basis_dimension,))
-    count_2 = 0
-    for boundary_name, nset in zip(nsets, nsets.values()):
+    iter_constrained_face = 0
+    # for boundary_name, nset in zip(nsets, nsets.values()):
+    for boundary_name, nset in zip(nsets_faces, nsets_faces.values()):
         for face_global_index in nset:
             face = faces[face_global_index]
             face_reference_frame_transformation_matrix = face.reference_frame_transformation_matrix
+            # ----------------------------------------------------------------------------------------------------------
+            m_psi_psi_face = Integration.get_face_mass_matrix_in_face(
+                face, face_basis, face_reference_frame_transformation_matrix
+            )
+            m_psi_psi_face_inv = np.linalg.inv(m_psi_psi_face)
+            # ----------------------------------------------------------------------------------------------------------
             displacement = boundary_conditions[boundary_name][0]
             for direction, displacement_component in enumerate(displacement):
                 if not displacement_component is None:
                     displacement_vector = Displacement(
                         face, face_basis, face_reference_frame_transformation_matrix, displacement_component
                     ).displacement_vector
+                    # if False:
+                    #     print("hello")
+                    #     displacement_vector = np.zeros((face_basis.basis_dimension,))
+                    #     if displacement_component(0) != 0:
+                    #         displacement_vector[0] = 1.0
+                    #     print(displacement_vector)
                     # --------------------------------------------------------------------------------------------------
-                    l0 = count_2 * face_basis.basis_dimension
-                    l1 = (count_2 + 1) * face_basis.basis_dimension
+                    displacement_vector = m_psi_psi_face_inv @ displacement_vector
+                    # --------------------------------------------------------------------------------------------------
+                    l0 = iter_constrained_face * face_basis.basis_dimension
+                    l1 = (iter_constrained_face + 1) * face_basis.basis_dimension
                     c0 = (
                         face_global_index * unknown.field_dimension * face_basis.basis_dimension
                         + direction * face_basis.basis_dimension
@@ -320,9 +341,11 @@ def solve(
                     lagrange_multiplyer_matrix[l0:l1, c0:c1] += np.eye(face_basis.basis_dimension)
                     # --------------------------------------------------------------------------------------------------
                     h_vector[l0:l1] += displacement_vector
-                    count_2 += 1
+                    iter_constrained_face += 1
+    print("h_vector : \n{}".format(h_vector))
+    print("lagrange_multiplyer_matrix : \n{}".format(lagrange_multiplyer_matrix))
     # ------------------------------------------------------------------------------------------------------------------
-    double_lagrange = True
+    double_lagrange = False
     # ------------------------------------------------------------------------------------------------------------------
     # If a single Lagrange multiplyier is used to enforce Dirichlet boundary conditions
     # ------------------------------------------------------------------------------------------------------------------
@@ -424,13 +447,14 @@ def solve(
         c0 = total_system_size + (number_of_constrained_faces * face_basis.basis_dimension)
         c1 = total_system_size + 2 * (number_of_constrained_faces * face_basis.basis_dimension)
         global_matrix_2[l0:l1, c0:c1] += id_lag
+    # print("global_matrix_2 : \n{}".format(global_matrix_2))
+    print("global_vector_2 : \n{}".format(global_vector_2))
     # ------------------------------------------------------------------------------------------------------------------
     # Solving the global system for faces unknowns
     # ------------------------------------------------------------------------------------------------------------------
-    print("global_matrix : \n{}".format(global_matrix_2))
+    # print("global_matrix : \n{}".format(global_matrix_2))
     global_solution = np.linalg.solve(global_matrix_2, global_vector_2)
-    # ------------------------------------------------------------------------------------------------------------------
-    unknowns_at_faces = global_solution[:total_system_size]
+    print("global_solution : \n{}".format(global_solution))
     # ------------------------------------------------------------------------------------------------------------------
     # Getting the number of vertices
     # ------------------------------------------------------------------------------------------------------------------
@@ -442,12 +466,41 @@ def solve(
     # ------------------------------------------------------------------------------------------------------------------
     quadrature_points = np.zeros((number_of_quadrature_points, unknown.problem_dimension))
     unknowns_at_vertices = [np.zeros((number_of_vertices,)) for i in range(unknown.field_dimension)]
+    f_unknowns_at_vertices = [np.zeros((number_of_vertices,)) for i in range(unknown.field_dimension)]
     unknowns_at_quadrature_points = [np.zeros((number_of_quadrature_points,)) for i in range(unknown.field_dimension)]
     vertices_weights = np.zeros((number_of_vertices,))
+    f_vertices_weights = np.zeros((number_of_vertices,))
     # ------------------------------------------------------------------------------------------------------------------
     # Desassembly
     # ------------------------------------------------------------------------------------------------------------------
     quadrature_point_count = 0
+    # ==================================================================================================================
+    # ==================================================================================================================
+    faces_indices = range(number_of_faces)
+    for face_index in faces_indices:
+        face_vertices_connectivity_matrix = faces_vertices_connectivity_matrix[face_index]
+        face_vertices = vertices[face_vertices_connectivity_matrix]
+        face = faces[face_index]
+        l0 = face_index * unknown.field_dimension * face_basis.basis_dimension
+        l1 = (face_index + 1) * unknown.field_dimension * face_basis.basis_dimension
+        x_face = global_solution[l0:l1]
+        for i, vertex in enumerate(face_vertices):
+            vertex_in_face = Face.get_points_in_face_reference_frame(vertex, face.reference_frame_transformation_matrix)
+            centroid_in_face = Face.get_points_in_face_reference_frame(
+                face.centroid, face.reference_frame_transformation_matrix
+            )
+            v = face_basis.get_phi_vector(vertex_in_face, centroid_in_face, face.volume)
+            for direction in range(unknown.field_dimension):
+                l0 = direction * face_basis.basis_dimension
+                l1 = (direction + 1) * face_basis.basis_dimension
+                vertex_value_vector = v * x_face[l0:l1]
+                global_index = face_vertices_connectivity_matrix[i]
+                l0 = global_index
+                l1 = global_index + 1
+                f_unknowns_at_vertices[direction][l0:l1] += np.sum(vertex_value_vector)
+                f_vertices_weights[l0:l1] += 1.0
+    # ==================================================================================================================
+    # ==================================================================================================================
     for cell_index in cells_indices:
         # --------------------------------------------------------------------------------------------------------------
         # Getting faces unknowns
@@ -500,9 +553,11 @@ def solve(
     # ------------------------------------------------------------------------------------------------------------------
     for direction in range(unknown.field_dimension):
         unknowns_at_vertices[direction] = unknowns_at_vertices[direction] / vertices_weights
+        f_unknowns_at_vertices[direction] = f_unknowns_at_vertices[direction] / f_vertices_weights
     return (
         (vertices, unknowns_at_vertices),
         (quadrature_points, unknowns_at_quadrature_points),
+        (vertices, f_unknowns_at_vertices),
         # unknowns_at_faces
     )
 

test_cases/pythhon3d_2d0.py
@@ -19,42 +19,43 @@ d = 2
 face_polynomial_order = 1
 cell_polynomial_order = 2
 field_dimension = 1
-stabilization_parameter = 1.0  # K2
+stabilization_parameter = 10.0  # K2
+# stabilization_parameter = 1000000.0  # K2
 mesh_file = "/Users/davidsiedel/Projects/PytHHOn3D/meshes/mesh2D25.geof"
+# mesh_file = "/Users/davidsiedel/Projects/PytHHOn3D/meshes/triangles_test.geof"
 operator_type = "HDG"
 #
-pressure_left = [lambda x: 0.0, lambda x: 0.0]
-pressure_right = [lambda x: 0.0, lambda x: 0.0]
-pressure_top = [lambda x: 0.0, lambda x: 0.0]
-pressure_bottom = [lambda x: 0.0, lambda x: 0.0]
+pressure_left = [lambda x: 0.0]
+pressure_right = [lambda x: 0.0]
+pressure_top = [lambda x: 0.0]
+pressure_bottom = [lambda x: 0.0]
 #
-displacement_left = [lambda x: 0.0, lambda x: 0.0]
-displacement_right = [lambda x: 0.0, lambda x: 0.0]
-displacement_right = [lambda x: 0.0, lambda x: 0.0]
-displacement_top = [lambda x: 0.0, lambda x: 0.0]
-displacement_bottom = [lambda x: 0.0, lambda x: 0.0]
+displacement_left = [None]
+displacement_right = [None]
+displacement_top = [lambda x: 1.0]
+displacement_bottom = [lambda x: 0.0]
 #
-load = [
-    lambda x: np.sin(2.0 * np.pi * x[0]) * np.sin(2.0 * np.pi * x[1]),
-    lambda x: np.sin(2.0 * np.pi * x[0]) * np.sin(2.0 * np.pi * x[1]),
-]
-#
+
 lam = 10.0
 load = [
     lambda x: (0.2 * np.sin(2.0 * np.pi * x[1])) * (np.cos(2.0 * np.pi * x[0]) - 1.0)
-    + (np.sin(2.0 * np.pi * x[0]) * np.sin(2.0 * np.pi * x[1])) / (5.0 + 5.0 * lam),
-    lambda x: (-0.2 * np.sin(2.0 * np.pi * x[1])) * (np.cos(2.0 * np.pi * x[0]) - 1.0)
     + (np.sin(2.0 * np.pi * x[0]) * np.sin(2.0 * np.pi * x[1])) / (5.0 + 5.0 * lam),
 ]
 #
 load = [
     lambda x: 6.0 * x[0] * x[1] * (1.0 - x[1]) - 2.0 * x[0] ** 3,
-    lambda x: 0.0,
 ]
 #
 load = [
-    lambda x: 1.0,
-    lambda x: 0.0,
+    lambda x: -0.0,
+]
+#
+load = [
+    lambda x: np.sin(2.0 * np.pi * x[0]) * np.sin(2.0 * np.pi * x[1]),
+]
+#
+load = [
+    lambda x: -10.0,
 ]
 #
 boundary_conditions = {
@@ -73,15 +74,21 @@ boundary_conditions = {
     cells_vertices_connectivity_matrix,
     faces_vertices_connectivity_matrix,
     nsets,
+    flags,
     cell_basis,
     face_basis,
     unknown,
 ) = build(mesh_file, field_dimension, face_polynomial_order, cell_polynomial_order, operator_type)
 # ------------------------------------------------------------------------------------------------------------------
 d = unknown.problem_dimension
-tangent_matrices = [np.eye(d ** 2) for i in range(len(cells))]
+tangent_matrices = [np.eye(2) for i in range(len(cells))]
+# tangent_matrices = [np.array([[1.0, 0.0], [0.0, 1.0]]) for i in range(len(cells))]
 # ------------------------------------------------------------------------------------------------------------------
-(vertices, unknowns_at_vertices), (quadrature_points, unknowns_at_quadrature_points) = solve(
+(
+    (vertices, unknowns_at_vertices),
+    (quadrature_points, unknowns_at_quadrature_points),
+    (f_vertices, f_unknowns_at_vertices),
+) = solve(
     vertices,
     faces,
     cells,
@@ -90,6 +97,7 @@ tangent_matrices = [np.eye(d ** 2) for i in range(len(cells))]
     cells_vertices_connectivity_matrix,
     faces_vertices_connectivity_matrix,
     nsets,
+    flags,
     cell_basis,
     face_basis,
     unknown,
@@ -99,23 +107,69 @@ tangent_matrices = [np.eye(d ** 2) for i in range(len(cells))]
     load,
 )
 # ------------------------------------------------------------------------------------------------------------------
-x, y = vertices.T
-plt.tricontourf(x, y, unknowns_at_vertices[0])
-plt.colorbar()
-plt.show()
-#
-import numpy as np
-import matplotlib.pyplot as plt
+scatter = False
+if scatter:
+    marker_size = 50
+    plt.scatter(f_vertices[:, 0], f_vertices[:, 1], marker_size, c=f_unknowns_at_vertices[0])
+    # plt.scatter(vertices[:, 0], vertices[:, 1], marker_size, c=unknowns_at_vertices[0])
+    cbar = plt.colorbar()
+    plt.show()
 
-x = np.linspace(0.0, 1.0, 100)
-y = np.linspace(0.0, 1.0, 100)
-X, Y = np.meshgrid(x, y)
+    plt.scatter(vertices[:, 0], vertices[:, 1], marker_size, c=unknowns_at_vertices[0])
+    # plt.scatter(vertices[:, 0], vertices[:, 1], marker_size, c=unknowns_at_vertices[0])
+    cbar = plt.colorbar()
+    plt.show()
+else:
+    # x, y = f_vertices.T
+    # plt.tricontourf(x, y, f_unknowns_at_vertices[0], levels=1000)
+    # plt.colorbar()
+    # plt.show()
+    #
+    # fig, ax = plt.subplots(1, 2, sharex=True, sharey=True)
+    # fig, axes = plt.subplots(nrows=2, ncols=2)
+    # for ax in axes.flat:
+    #     im = plt.tricontourf(x, y, f_unknowns_at_vertices[0], levels=1000)
 
-# Z = (1 - X / 2 + X ** 5 + Y ** 3) * np.exp(-(X ** 2) - Y ** 2)  # calcul du tableau des valeurs de Z
-Z = np.sin(2.0 * np.pi / 0.9 * X) * np.sin(2.0 * np.pi / 0.3 * Y)
-Z = Y * (1.0 - Y) * (X ** 3)
+    # fig.subplots_adjust(right=0.8)
+    # cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.7])
+    # fig.colorbar(im, cax=cbar_ax)
+    # plt.show()
+    #
+    x, y = vertices.T
+    plt.tricontourf(x, y, unknowns_at_vertices[0], levels=1000)
+    plt.colorbar()
+    plt.show()
+    #
+    x = np.linspace(0.0, 1.0, 100)
+    y = np.linspace(0.0, 1.0, 100)
+    X, Y = np.meshgrid(x, y)
+    Z = 6.0 * Y - 5.0 * Y ** 2
+    plt.pcolor(X, Y, Z)
+    plt.colorbar()
+    plt.show()
 
-plt.pcolor(X, Y, Z)
-plt.colorbar()
+# z = unknowns_at_vertices[0]
+# scaled_z = (z - z.min()) / z.ptp()
+# colors = plt.cm.coolwarm(scaled_z)
+# plt.scatter(vertices[:, 0], vertices[:, 1], c=colors, s=100, linewidths=4)
+# # for pX, pY, val in zip(vertices[:, 0], vertices[:, 1], unknowns_at_vertices[0]):
+# #     plt.scatter(pX, pY, val)
 
-plt.show()
+# plt.colorbar()
+# plt.show()
+# #
+# import numpy as np
+# import matplotlib.pyplot as plt
+
+# x = np.linspace(0.0, 1.0, 100)
+# y = np.linspace(0.0, 1.0, 100)
+# X, Y = np.meshgrid(x, y)
+
+# # Z = (1 - X / 2 + X ** 5 + Y ** 3) * np.exp(-(X ** 2) - Y ** 2)  # calcul du tableau des valeurs de Z
+# Z = np.sin(2.0 * np.pi / 0.9 * X) * np.sin(2.0 * np.pi / 0.3 * Y)
+# Z = Y * (1.0 - Y) * (X ** 3)
+
+# plt.pcolor(X, Y, Z)
+# plt.colorbar()
+
+# # plt.show()